0aa9f93c43d76bc7c0f900e4ecaf72ae81bb4a9f
Sheila Wittman <szandora789@gmail.com>
2018-06-27 17:45:39 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 3, "M", "brown", "calm", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 1, "M", "black", "hyper", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 5, "F", "brown", "calm", 1);
